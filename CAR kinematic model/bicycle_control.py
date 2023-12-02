ebf9a36c54fe9a95ef67616e8a34d9162aea82c8
Lamfurst <lamfurst@outlook.com>
2023-12-02 16:57:58 -0500

    Modify code

--- CAR kinematic model/bicycle_control.py
+++ CAR kinematic model/bicycle_control.py
@@ -23,6 +23,23 @@ class Car_Dynamics:
         psi_dot = self.v*np.tan(delta)*np.cos(self.beta)/self.L
         return np.array([[x_dot, y_dot, psi_dot, v_dot]]).T
     
+    # This batch move is used in mppi controller
+    def batched_move(self, curr_state, accelerate, delta):
+        # Accelerate is a vector of size (batch_size, 1)
+        # delta is a vector of size (batch_size, 1)
+        # curr_state is a vector of size (batch_size, 4)
+
+        x = curr_state[:, 0]
+        y = curr_state[:, 1]
+        psi = curr_state[:, 2]
+        v = curr_state[:, 3]
+        beta = np.arctan((self.b * np.tan(delta)) / (self.L))
+        x_dot = v*np.cos(psi + beta)
+        y_dot = v*np.sin(psi + beta)
+        v_dot = accelerate
+        psi_dot = v*np.tan(delta)*np.cos(beta)/self.L
+        return np.hstack([x_dot, y_dot, psi_dot, v_dot])
+    
     # def fn(self, accelerate, delta):
     #     beta = np.arctan((self.b * np.tan(delta)) / (self.L))
     #     x_dot = self.v*np.cos(self.psi + self.beta)
@@ -58,6 +75,9 @@ class Car_Dynamics:
     
     def predict_state(self, u_k):
         return self.state + self.move(u_k[0], u_k[1])*self.dt
+    
+    def batch_predict_state(self, curr_state, u_k):
+        return curr_state + self.batched_move(curr_state, u_k[:,0], u_k[:,1])*self.dt
 
 
     
@@ -218,4 +238,136 @@ class Linear_MPC_Controller:
     
 
 ######################################################################################################################################################################
-class MPPI_controller
+class MPPIController(object):
+
+    def __init__(self, model, num_samples, horizon, hyperparams):
+        """
+
+        :param env: Simulation environment. Must have an action_space and a state_space.
+        :param num_samples: <int> Number of perturbed trajectories to sample
+        :param horizon: <int> Number of control steps into the future
+        :param hyperparams: <dic> containing the MPPI hyperparameters
+        """
+        self.model = model
+        self.T = horizon
+        self.K = num_samples
+        self.lambda_ = hyperparams['lambda']
+        self.action_size = 2
+        self.state_size = 4
+        self.goal_state = np.zeros(self.state_size)  # This is just a container for later use
+        self.Q = hyperparams['Q'] # Quadratic Cost Matrix (state_size, state_size)
+        self.noise_mu = np.zeros(self.action_size)
+        self.noise_sigma = hyperparams['noise_sigma']  # Noise Covariance matrix shape (action_size, action_size)
+        self.noise_sigma_inv = np.linalg.inv(self.noise_sigma)
+        self.U = np.zeros((self.T, self.action_size)) # nominal action sequence (T, action_size)
+        self.u_init = np.zeros(self.action_size)
+        self.noise_dist = np.random.multivariate_normal(self.noise_mu, self.noise_sigma)
+
+    def reset(self):
+        """
+        Resets the nominal action sequence
+        :return:
+        """
+        self.U = np.zeros((self.T, self.action_size))# nominal action sequence (T, action_size)
+
+    def command(self, state):
+        """
+        Run a MPPI step and return the optimal action.
+        :param state: torch tensor of shape (state_size,)
+        :return:
+        """
+        action = None
+        # Need to modify perturbations
+        perturbations = self.noise_dist.sample((self.K, self.T))    # shape (K, T, action_size)
+        perturbed_actions = self.U + perturbations      # shape (K, T, action_size)
+        trajectory = self._rollout_dynamics(state, actions=perturbed_actions)
+        trajectory_cost = self._compute_trajectory_cost(trajectory, perturbations)
+        self._nominal_trajectory_update(trajectory_cost, perturbations)
+        # select optimal action
+        action = self.U[0]
+        # final update nominal trajectory
+        self.U = torch.roll(self.U, -1, dims=0) # move u_t to u_{t-1}
+        self.U[-1] = self.u_init # Initialize new end action
+        return action
+
+    def _rollout_dynamics(self, state_0, actions):
+        """
+        Roll out the environment dynamics from state_0 and taking the control actions given by actions
+        :param state_0: torch tensor of shape (state_size,)
+        :param actions: torch tensor of shape (K, T, action_size)
+        :return:
+         * trajectory: torch tensor of shape (K, T, state_size) containing the states along the trajectories given by
+                       starting at state_0 and taking actions.
+                       This tensor contains K trajectories of T length.
+         TIP 1: You may need to call the self._dynamics method.
+         TIP 2: At most you need only 1 for loop.
+        """
+        state = np.expand_dims(state_0, axis=0).repeat(self.K, 1) # transform it to (K, state_size)
+        trajectory = None
+        # --- Your code here
+        trajectory = np.zeros(self.K, self.T, self.state_size)
+        trajectory[:,0,:] = self._dynamics(state, actions[:,0,:])
+        for t in range(1, self.T):
+          trajectory[:,t,:] = self._dynamics(trajectory[:,t-1,:], actions[:,t,:])
+        # ---
+        return trajectory
+
+    def _compute_trajectory_cost(self, goal_state, trajectory, perturbations):
+        """
+        Compute the costs for the K different trajectories
+        :param trajectory: torch tensor of shape (K, T, state_size)
+        :param perturbations: torch tensor of shape (K, T, action_size)
+        :return:
+         - total_trajectory_cost: torch tensor of shape (K,) containing the total trajectory costs for the K trajectories
+        Observations:
+        * The trajectory cost be the sum of the state costs and action costs along the trajectories
+        * State cost should be quadratic as (state_i-goal_state)^T Q (state_i-goal_state)
+        * Action costs should be given by (non_perturbed_action_i)^T noise_sigma^{-1} (perturbation_i)
+
+        TIP 1: the nominal actions (without perturbation) are stored in self.U
+        TIP 2: Check Algorithm 2 in https://ieeexplore.ieee.org/document/7989202 for more references.
+        """
+        total_trajectory_cost = None
+
+        non_bias_traj = trajectory - goal_state # (K, T, state_size)
+        # state_cost = torch.bmm(non_bias_traj @ self.Q, non_bias_traj.transpose(1,2)) # (K, T, T)
+        # state_cost = torch.einsum("ijj -> i", state_cost)
+        state_cost = np.einsum(non_bias_traj @ self.Q, non_bias_traj.transpose(1,2)) # (K, T, T)
+        state_cost = np.einsum("ijj -> i", state_cost)
+
+        action_cost = self.lambda_ * self.U @ self.noise_sigma_inv @ perturbations.transpose(1,2)
+        action_cost = torch.einsum("ijj -> i", action_cost)
+        total_trajectory_cost = action_cost + state_cost
+
+        return total_trajectory_cost
+
+    def _nominal_trajectory_update(self, trajectory_costs, perturbations):
+        """
+        Update the nominal action sequence (self.U) given the trajectory costs and perturbations
+        :param trajectory_costs: torch tensor of shape (K,)
+        :param perturbations: torch tensor of shape (K, T, action_size)
+        :return: No return, you just need to update self.U
+
+        TIP: Check Algorithm 2 in https://ieeexplore.ieee.org/document/7989202 for more references about the action update.
+        """
+        # --- Your code here
+        beta = torch.min(trajectory_costs)
+        gamma = torch.exp(- 1 / self.lambda_ * (trajectory_costs - beta))
+        eta = torch.sum(gamma)
+        omega = 1 / eta * gamma
+
+        self.U += torch.einsum("i,ijk -> jk", omega, perturbations)
+        # ---
+
+    def _dynamics(self, state, accelrate, delta):
+        """
+        Query the environment dynamics to obtain the next_state in a batched format.
+        :param state: torch tensor of size (...., state_size)
+        :param action: torch tensor of size (..., action_size)
+        :return: next_state: torch tensor of size (..., state_size)
+        """
+        # next_state = self.env.batched_dynamics(state.cpu().detach().numpy(), action.cpu().detach().numpy())
+        # next_state = torch.tensor(next_state, dtype=state.dtype)
+        state_dot = self.model.batch_move(state, accelrate, delta)
+        next_state = self.model.batch_predict_state(state, state_dot)
+        return next_state
